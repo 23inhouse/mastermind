523f15724a6ecf1670f12444a33cb0b348c50b32
Benjamin Lewis <23inhouse@gmail.com>
2020-01-02 13:45:04 +0100

    Add base project

---
+++ MasterMind/MasterMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterMind
+//
+//  Created by Benjamin Lewis on 2/1/20.
+//  Copyright © 2020 Benjamin Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
